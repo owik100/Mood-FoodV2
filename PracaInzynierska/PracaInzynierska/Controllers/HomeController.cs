a7bafaf25c3401bbdbfc0633273e0a9f8c007df5
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-05-17 23:54:08 +0200

    Dodano obrazki. Tworzenie wyglądu indexu

--- PracaInzynierska/PracaInzynierska/Controllers/HomeController.cs
+++ PracaInzynierska/PracaInzynierska/Controllers/HomeController.cs
@@ -4,15 +4,30 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PracaInzynierska.Data;
 using PracaInzynierska.Models;
+using PracaInzynierska.Models.Entities;
 
 namespace PracaInzynierska.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+
+            List<Product> randomProducts;
+            randomProducts = _db.Products
+                .Where(x => x.Category.ShowProductsFromTheseCategoryInHomePage == true)
+                .OrderBy(x => Guid.NewGuid())
+                .Take(4).ToList();
+
+            return View(randomProducts);
         }
 
         public IActionResult Privacy()
---
+++ PracaInzynierska/PracaInzynierska/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracaInzynierska.Infrastructure
+{
+    public static class UrlExtensions
+    {
+
+        public static string ImagePath(this IUrlHelper helper, string imageName)
+        {
+            string path = ConfigurationManager.AppSetting["ImagePath"];
+            return Path.Combine(path + imageName);
+        }
+
+        public static string ProductImagePath(this IUrlHelper helper, string imageName)
+        {
+            string path = ConfigurationManager.AppSetting["ProductsImagePath"];
+            return Path.Combine(path + imageName);
+        }
+
+        public static string CategoriesImagePath(this IUrlHelper helper, string imageName)
+        {
+            string path = ConfigurationManager.AppSetting["CategoriesImagePath"];
+            return Path.Combine(path + imageName);
+        }
+    }
+}

3285c63e00df2c5dc39aefdd6b7e70d6b72aca06
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-05-23 20:42:15 +0200

    Dodano Komponent nawigacji menu

---
+++ PracaInzynierska/PracaInzynierska/Components/CategoryNavigationViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracaInzynierska.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracaInzynierska.Components
+{
+    public class CategoryNavigationViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryNavigationViewComponent(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _db.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}

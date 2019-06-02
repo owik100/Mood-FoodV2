df6306e9b707c5e0175795508712f4b7e56bd9e7
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-06-02 13:13:05 +0200

    Nowa strona rejestracji

--- PracaInzynierska/PracaInzynierska/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PracaInzynierska/PracaInzynierska/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -55,6 +55,42 @@ namespace PracaInzynierska.Areas.Identity.Pages.Account
             [Display(Name = "Powtórz Hasło")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Display(Name = "Imię")]
+            [Required(ErrorMessage = "Podaj imię")]
+            [StringLength(50)]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Nazwisko")]
+            [Required(ErrorMessage = "Podaj nazwisko")]
+            [StringLength(50)]
+            public string LastName { get; set; }
+
+            [Display(Name = "Miasto")]
+            [Required(ErrorMessage = "Podaj miasto")]
+            [StringLength(50)]
+            public string City { get; set; }
+
+            [Display(Name = "Ulica")]
+            [Required(ErrorMessage = "Podaj ulicę")]
+            [StringLength(50)]
+            public string Street { get; set; }
+
+            [Display(Name = "Kod pocztowy")]
+            [Required(ErrorMessage = "Podaj kod pocztowy")]
+            [StringLength(6)]
+            public string ZIPCode { get; set; }
+
+            [Display(Name = "Numer domu/mieszkania")]
+            [Required(ErrorMessage = "Podaj numer domu/mieszkania")]
+            [StringLength(10)]
+            public string HouseNumber { get; set; }
+
+            [Display(Name = "Numer telefonu")]
+            [Required(ErrorMessage = "Podaj numer telefonu")]
+            [DataType(DataType.PhoneNumber)]
+            public string PhoneNumber { get; set; }
+            
         }
 
         public void OnGet(string returnUrl = null)
@@ -67,7 +103,17 @@ namespace PracaInzynierska.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    City = Input.City,
+                    Street = Input.Street,
+                    ZIPCode = Input.ZIPCode,
+                    HouseNumber = Input.HouseNumber,
+                    PhoneNumber = Input.PhoneNumber};
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
